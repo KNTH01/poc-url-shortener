c93aa577d8a27c4ab93ff39e71ae9df68374ffb7
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2016-07-15 17:01:34 +0200

    remove db from config

--- config.js
+++ config.js
@@ -2,10 +2,5 @@
 
 module.exports = {
   // the URL shortening host - shortened URLs will be this + base58 ID (ex: http://localhost:3000/3Ys)
-  webhost: 'http://localhost:3000/',
-  db: {
-    // MongoDB conf
-    host: 'localhost',
-    name: 'url_shortener'
-  }
+  webhost: 'http://localhost:3000/'
 }
