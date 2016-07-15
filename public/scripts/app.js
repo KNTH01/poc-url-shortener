b926f4a8a34c99b1a98a08d179b9dffe3268a40f
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2016-07-15 14:58:12 +0200

    play with styles

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,5 @@
 // add an event listener to the shorten button for when the user clicks it
-$('#shorten-cta').on('click', () => {
+$('.FormInput-button').on('click', () => {
   console.log('Shorten !')
     // AJAX call to /api/shorten with the URL that the user entered in the input box
   $.ajax({
@@ -7,19 +7,19 @@ $('#shorten-cta').on('click', () => {
     type: 'POST',
     dataType: 'JSON',
     data: {
-      url: $('#url-field').val()
+      url: $('.FormInput-input').val()
     },
     success: (data) => {
       // display the shortened URL to the user that is returned by the server
       let resultHTML = '<a class="result" href="' + data.shortUrl + '">' + data.shortUrl + '</a>'
-      $('#link').html(resultHTML)
-      $('#link').hide().fadeIn('slow')
+      $('.Result').html(resultHTML)
+      $('.Result').hide().fadeIn('slow')
     },
     error: (data) => {
-      let resultHTML = `<p>${data.responseText}</p>`
+      let resultHTML = `<p class="ErrorMsg">${data.responseText}</p>`
       console.log(data)
-      $('#link').html(resultHTML)
-      $('#link').hide().fadeIn('slow')
+      $('.Result').html(resultHTML)
+      $('.Result').hide().fadeIn('slow')
     }
   })
 })
