f93290fe8437ee6f50a8d24c0b68537157acff16
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2016-07-19 14:42:51 +0200

    add sequelize and playing with it

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
-
+const Sequelize = require('sequelize')
 const redis = require('redis')
+
+const config = require('./config')
+
+// Create Sequelize connection with default options
+const sequelize = new Sequelize(config.database.dbname, config.database.username, config.database.password)
+
+// Create Redis connection
 const redisClient = redis.createClient()
 
 // Store this as global in order to use it in routes.js
@@ -27,9 +34,55 @@ app.get('/', (req, res) => {
 
 app.use('/', routes)
 
-// Wait for connection to Redis server before launching the server
-redisClient.on('connect', () => {
-  app.listen(3000, () => {
-    console.log('Server listening on port 3000')
+// Wait for connection to our Databases (Redis and MySQL) before launching the server
+sequelize
+  .authenticate()
+  .then((err) => {
+    if (err) {
+      console.error(err)
+    }
+    console.log('Connection to MySQL has been established successfully.')
+    app.listen(config.server.port, () => {
+      console.log(`Serveur is up to address: http://${config.server.host}:${config.server.port}/`)
+    })
   })
+  .catch((err) => {
+    if (err) {
+      console.log('Unable to connect to the database:', err)
+      throw err
+    }
+  })
+
+redisClient.on('connect', (err) => {
+  if (err) {
+    console.error(err)
+    throw err
+  }
+  console.log('Connection to Redis has been established successfully.')
 })
+
+// const Url = sequelize.define('url', {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     primaryKey: true,
+//     autoIncrement: true
+//   },
+//   longUrl: {
+//     type: Sequelize.STRING,
+//     field: 'long_url'
+//   },
+//   shortUrl: {
+//     type: Sequelize.STRING,
+//     field: 'short_url'
+//   }
+// })
+//
+// Url.sync({
+//   // force: true
+// }).then(() => {
+//   // Table created
+//   return Url.create({
+//     longUrl: 'Ok',
+//     shortUrl: 'K'
+//   })
+// })
--- config.js
+++ config.js
@@ -2,5 +2,14 @@
 
 module.exports = {
   // the URL shortening host - shortened URLs will be this + base58 ID (ex: http://localhost:3000/3Ys)
-  webhost: 'http://localhost:3000/'
+  webhost: 'http://localhost:3000/',
+  server: {
+    host: 'localhost',
+    port: 3000
+  },
+  database: {
+    dbname: 'poc_url_shortener',
+    username: 'root',
+    password: 'root'
+  }
 }
